a41800919f5d44d7112d582738880cb41af33e07
Sourav <souravghosh251@outlook.com>
2026-05-12 08:47:52 +0530

    adding changes

--- sql_udemy_queries/01_sql.sql
+++ sql_udemy_queries/01_sql.sql
@@ -5,3 +5,9 @@ select * from orders
 /* Select specific columns */
 
 select first_name , country , score from customers
+
+/* filtering data using where clause */
+
+select * from customers where score <> 0
+
+select * from customers where country='Germany'
